d6ca35846e7c0bd34e645a13cfe820dc2d43107b
Sanjeev Pandit <mesanjeevpandit68@gmail.com>
2022-12-29 18:30:10 +0545

    login & registration form

---
+++ News/news1.php
@@ -0,0 +1,99 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+    <title>News1</title>
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col">
+                <h1 class="d-flex justify-content-center" style="background-color: rgb(215, 208, 208); height: 200px;">News</h1>
+            </div>
+            
+        </div>
+    </div>
+
+    <div class="container" style=" margin-top:80px">
+        <div class="row">
+            <div class="col">
+                <div class="section-title">
+                  <a href="./pdf/DIT.pdf" style="text-decoration: none; font-size: 20px;"> <p style="color:rgb(9, 119, 152)"> विभिन्न छाेटाे अवधि तालिमकाे अन्तर्वार्ताकाे नतिजा सम्बन्धी सूचना !</a>
+                        <a href="./pdf/DIT.pdf" style="background-color: black; 
+                    color: white; 
+                    border-radius:20px; 
+                    padding:5px 5px;
+                    text-decoration: none;">Download</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+<footer class="bg-dark text-white pt-5 pb-4 mt-5">
+    <div class="container text-center text-md-left">
+      <div class="row text-center text-md-left">
+
+        <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+          <h5 class="text-uppercase font-weight-bold text-warning">NBPI</h5>
+          <a href="#">
+            <i class="bi bi-envelope text-light"></i> nbpi.edu@gmail.com
+          </a>
+          <a href="#">
+            <i class="bi bi-telephone ps-2 text-light"></i> 011864290
+          </a>
+          <a href="#"><i class="bi bi-geo-alt-fill text-light"></i> Nepal Banepa Polytechnic Institute, Banepa,
+            Kavre</a>
+        </div>
+
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+          <h5 class="text-uppercase mb-4 font-weight-bold text-warning">
+            Pages
+          </h5>
+          <a href="/nav.html">Home</a>
+          <a href="/about us/about-us.html">About</a>
+          <a href="/contact us/contact.html">Contact</a>
+          <a href="/downloads-button/download.html">Downloads</a>
+        </div>
+
+        <!-- <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+          <h5 class="text-uppercase mb-4 font-weight-bold text-warning">
+            Services
+          </h5>
+          <a href="#">Student Life</a>
+          <a href="#">Gallery</a>
+          <a href="#">Alumini</a>
+        </div> -->
+
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+          <h5 class="text-uppercase mb-4 font-weight-bold text-warning">
+            Quick Links
+          </h5>
+          <a href="http://www.ctevt.org.np/">
+            CTEVT (Council for Technical Education and Vocational Training)
+          </a>
+          <a href="http://titi.org.np/">
+            TITI (Training Institute for Technical Instruction)
+          </a>
+          <a href="http://nbpi.edu.np/tslc.php">
+            Tracer Study Questionaire
+          </a>
+        </div>
+
+        <hr style="line-height: 5px; color: white; width: 100%" />
+
+        <div class="row align-item-center">
+          <p class="mb-0">Copyright &copy 2022 All Right Reserved By: <strong>NBPI</strong></p>
+        </div>
+      </div>
+    </div>
+  </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
